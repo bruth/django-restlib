74a7bf49524d8c901e9a62d941ce1b3e9545b35d
Byron Ruth <bruth@codeomics.com>
2011-05-25 15:49:21 -0400

    added map of all HttpStatusCodes by code

--- restlib/http.py
+++ restlib/http.py
@@ -1,7 +1,7 @@
 class HttpMethod(object):
     """An HTTP method which defines whether this method is safe and
     idempotent.
-    
+
     ref: http://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html
     """
     def __init__(self, method, safe, idempotent):
@@ -63,7 +63,7 @@ methods = dict((str(x), x) for x in
 
 # Informational 1xx
 CONTINUE = HttpStatusCode(100, 'Continue')
-SWITCHING_PROTOCOLS = HttpStatusCode(101, 'Switch Protocols')
+SWITCHING_PROTOCOLS = HttpStatusCode(101, 'Switching Protocols')
 
 # Successful 2xx
 OK = HttpStatusCode(200, 'OK')
@@ -111,3 +111,47 @@ BAD_GATEWAY = HttpStatusCode(502, 'Bad Gateway')
 SERVICE_UNAVAILABLE = HttpStatusCode(503, 'Service Unavailable')
 GATEWAY_TIMEOUT = HttpStatusCode(504, 'Gateway Timeout')
 HTTP_VERSION_NOT_SUPPORTED = HttpStatusCode(505, 'HTTP Version Not Supported')
+
+responses = {
+    100: CONTINUE,
+    101: SWITCHING_PROTOCOLS,
+    200: OK,
+    201: CREATED,
+    202: ACCEPTED,
+    203: NON_AUTHORITATIVE_INFORMATION,
+    204: NO_CONTENT,
+    205: RESET_CONTENT,
+    206: PARTIAL_CONTENT,
+    300: MULTIPLE_CHOICES,
+    301: MOVED_PERMANENTLY,
+    302: FOUND,
+    303: SEE_OTHER,
+    304: NOT_MODIFIED,
+    305: USE_PROXY,
+    # 306: UNKNOWN,
+    307: TEMPORARY_REDIRECT,
+    400: BAD_REQUEST,
+    401: UNAUTHORIZED,
+    402: PAYMENT_REQUIRED,
+    403: FORBIDDEN,
+    404: NOT_FOUND,
+    405: METHOD_NOT_ALLOWED,
+    406: NOT_ACCEPTABLE,
+    407: PROXY_AUTHENTICATION_REQUIRED,
+    408: REQUEST_TIMEOUT,
+    409: CONFLICT,
+    410: GONE,
+    411: LENGTH_REQUIRED,
+    412: PRECONDITION_FAILED,
+    413: REQUEST_ENTITY_TOO_LARGE,
+    414: REQUEST_URI_TOO_LONG,
+    415: UNSUPPORTED_MEDIA_TYPE,
+    416: REQUESTED_RANGE_NOT_SATISFIABLE,
+    417: PRECONDITION_FAILED,
+    500: INTERNAL_SERVER_ERROR,
+    501: NOT_IMPLEMENTED,
+    502: BAD_GATEWAY,
+    503: SERVICE_UNAVAILABLE,
+    504: GATEWAY_TIMEOUT,
+    505: HTTP_VERSION_NOT_SUPPORTED,
+}
